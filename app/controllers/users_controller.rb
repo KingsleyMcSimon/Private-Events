6bc8f05812844ed158c2c758e9f1b1aeff632cdf
Ubuntu <ubuntu@ip-172-31-39-224.us-east-2.compute.internal>
2020-02-28 17:44:38 +0000

    created the sign-up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new(user_params)
+    end
+    
+    def create
+      if @user = User.new(user_params)
+         @user.save
+         redirect_to @user
+      else
+         render :new
+    end
+    
+    def show
+        @user = User.find(user_params)
+        @eventable = @events
+        @events = @eventable.events
+        @event = Event.new
+    end
+    
+    private
+    
+    def user_params
+        params.require(:user).permit(:username)
+    end
 end
