23a146b8e26faa5ce3819104a9258eb4ca14e234
Ubuntu <ubuntu@ip-172-31-39-224.us-east-2.compute.internal>
2020-03-04 21:31:26 +0000

    added the event controller's routes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,20 @@
 class EventsController < ApplicationController
+    
+    def index
+    end
+    
+    def new
+    end
+    
+    def create
+    end
+    
+    def show
+    end
+    
+    private
+    
+    def event_params
+        params.require(:event).permit(:name,:description,:date,:location)
+    end
 end
