c635e39fc338136c9e9815f692a464a7234b33d3
Ubuntu <ubuntu@ip-172-31-39-224.us-east-2.compute.internal>
2020-03-05 15:46:21 +0000

    created the session and invitation controllers

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  
+  def create
+    user = User.find_by(id: params[:session][:id])
+    if user
+    log_in user redirect_to user
+    else 
+    flash.now[:danger] = 'No such User' 
+    render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
