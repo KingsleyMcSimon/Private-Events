743d07f89abb1f980ecd9ce920a2001fb6105eda
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-09-11 18:46:41 -0700

    sessions form refactored, and Sessions#create refactored, it was using bad combination of keys while searching for a user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,13 @@ class SessionsController < ApplicationController
    end
   
   def create
-    user = User.find_by(id: params[:session][:id])
+    user = User.find_by(username: params[:session][:username])
     if user
-    sign_in user
-    redirect_to user
+      sign_in user
+      redirect_to user
     else 
-    flash.now[:danger] = 'No such User Exit' 
-    render 'new'
+      flash.now[:danger] = 'No such User Exit'
+      render 'new'
     end
   end
 
