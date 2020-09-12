723b8ddefbb782757f5b81434409aa2a351d0e8c
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-09-11 19:04:43 -0700

    error dispatching the correct HTTP method for logut route

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # Sessions
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
   # Users
   resources :users
   get '/signup', to: 'users#new'
