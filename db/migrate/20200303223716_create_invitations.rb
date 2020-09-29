ab5ca63d0910828d2361f2bd96377f4af45aa7b8
kingsley McSimon Ogbonna <kingsley.mcsimon@gmail.com>
2020-09-28 23:55:55 -0700

    yarn.lock and the db folder updated

--- db/migrate/20200303223716_create_invitations.rb
+++ db/migrate/20200303223716_create_invitations.rb
@@ -6,7 +6,7 @@ class CreateInvitations < ActiveRecord::Migration[6.0]
       t.boolean :attending, default: false
       t.timestamps
     end
-     add_foreign_key :invitations, :events, column: :user_event_id
+    add_foreign_key :invitations, :events, column: :user_event_id
     add_foreign_key :invitations, :users, column: :event_user_id
   end
 end
